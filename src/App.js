8731108187a226211213c93acc96414a385ce440
Umang Poudyal <umangpoudyal@gmail.com>
2022-04-10 12:01:12 -0400

    Fixed random button

--- src/App.js
+++ src/App.js
@@ -1,27 +1,15 @@
 import './App.css';
 import React, {Component} from 'react';
 import Songs from './Songs';
-import {Howl, Howler} from 'howler';
 
-class App extends Component() {
-  playClip() {
-    console.log('sound')
-    const Sounds = new Howl({
-      src: ['/another.mp3']
-    })
-    Sounds.play()
-    console.log('sound')
-  }
+class App extends Component {
   render() {
-    Howler.volume(1.0)
   return (
     <div className="App">      
         <h1>Random Lyric Generator</h1>
         <div className="underline"></div>
-
         <div className="rqg-box">
           <Songs />
-          <button className="another" onClick={this.playClip} >Another One</button>
         </div>
 
         <div className='footer'>
--- src/Songs.js
+++ src/Songs.js
@@ -5,12 +5,24 @@ import { FaYoutube } from 'react-icons/fa';
 const Songs = () => {
     const [index, setIndex] = useState(Math.floor(Math.random()*11));
     const {lyric, artist, link, song, image} = lyrics[index]
+
+    const randomSong = () => {
+        let randomNumber = Math.floor(Math.random() * lyrics.length);
+        
+        setIndex(randomNumber);
+    }
+
+
+
     return <div className="song-info">
+        <button className="another" onClick={randomSong} >Another One</button>
+
         <img src={image} alt={artist}></img>
         <h2>{artist}</h2>
         <h3>{song}</h3>
         <p>"{lyric}"</p>
-        <a href={link} target="_blank"><FaYoutube /></a>
+        <a href={link} target="_blank"><FaYoutube /></a>        
+        
     </div>
 };
 
